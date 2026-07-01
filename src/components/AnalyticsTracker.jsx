bf0b42e40ffc716cce3a95315dc11972f87ae4a8
Anup Kumar <anup.indira@hotmail.com>
2026-07-01 10:18:51 +0530

    Added google analytics

---
+++ src/components/AnalyticsTracker.jsx
@@ -0,0 +1,16 @@
+// src/components/AnalyticsTracker.jsx
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { trackPageView } from "../utils/analytics";
+
+const AnalyticsTracker = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        trackPageView(location.pathname + location.search);
+    }, [location]);
+
+    return null;
+};
+
+export default AnalyticsTracker;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import AnalyticsTracker from "./components/AnalyticsTracker";
 import { CartProvider } from "./components/Cart/cartContext";
 import ScrollToTop from "./components/ScrollTop";
 import { AuthProvider } from "./contexts/AuthContexts";
 import "./styles.css";
+import { initGA } from "./utils/analytics";
 
+initGA();
 const queryClient = new QueryClient();
 
 console.log("origin:", window.location.origin);
@@ -18,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <HelmetProvider>
       <BrowserRouter>
+        <AnalyticsTracker />
         <ScrollToTop />
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
