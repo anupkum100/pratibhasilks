cc2328c57a401d71e5e74a1d4908e7c3948ecce9
Anup Kumar <anup.indira@hotmail.com>
2026-07-22 18:47:47 +0530

    Updated email id to srvice

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,3 +1,3 @@
-export const PS_EMAIL = "care@pratibhasilks.com"
+export const PS_EMAIL = "service@pratibhasilks.com"
 export const PS_PHONE = "+91 9730880398"
 export const PS_PHONE_WHATSAPP = "9730880398"
